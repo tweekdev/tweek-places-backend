58024161bc74c953ff2b0dfbd26557425808110f
tweekdev <theauantoine@theau.home>
2020-09-07 12:14:12 +0200

    make it better

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ mongoose
     { useCreateIndex: true, useUnifiedTopology: true, useNewUrlParser: true }
   )
   .then(() => {
-    app.listen(process.env.PORT || 5000);
+    app.listen(5000);
   })
   .catch((error) => {
     console.log(error);
